b30375722127acb33280028813639cbbf3220494
Cdgr247 <cdgr247@gmail.com>
2024-03-25 20:08:42 -0400

    right

---
+++ db.py
@@ -0,0 +1,58 @@
+# Temp! Do not need after we connect to db 
+
+# users = {
+#     1:{
+#         'id' : 1,
+#         'username' : 'bc',
+#         'email' : 'bc@ct.com'
+#     },
+#     2 : {
+#         'id' : 2,
+#         'username' : 'ds',
+#         'email' : 'ds@ct.com',
+#         'password': '123'
+#     }
+# }
+
+# posts = {
+#     '1' : {
+#         'author' : 2,
+#         'title' : 'Welcome to Flask',
+#         'body' : 'Welcome to our first full-stack app, it\'s SUPER easy and simple!'
+#     },
+#     '2' : {
+#         'author' : 1,
+#         'title' : 'Flask intro',
+#         'body' : 'I would\'nt say it is THAT simple'
+#     }
+# }
+
+
+cars = {
+    1:{
+        'id' : 1,
+        'model' : 'Honda',
+        'make' : 'Accord'
+    },
+    2 : {
+        'id' : 2,
+        'model' : 'Hyndai',
+        'make' : 'Elantra'
+    }
+}
+sale_receipts = {
+    1 : {
+        'car' : 2,
+        'condition' : 'New',
+        'color' : 'Blue',
+        'model' : 'Hyndai',
+        'make' : 'Elantra'
+    },
+    2 : {
+        'car' : 1,
+        'condition' : 'New',
+        'color' : 'Sonic Gray Pearl',
+        'model' : 'Honda',
+        'make' : 'Accord'
+    }
+}
